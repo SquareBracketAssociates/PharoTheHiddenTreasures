c1e9bd91ae1ff29c458fe73b70f9325e6a612fc1
StephaneDucasse <stephane.ducasse@inria.fr>
2014-12-30 10:57:59 +0100

    fixing @@authortodo ... we lost the definition somewhere

--- compile.sh
+++ compile.sh
@@ -72,7 +72,7 @@ function compile_latex_book() {
   echo COMPILING Book
   echo =========================================================
   cd book-result
-  produce_pdf . Book
+  produce_pdf . TheHiddenParts
 }
 
 function latex_enabled() {
